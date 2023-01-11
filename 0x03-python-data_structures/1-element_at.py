919469990957d38eaa0828ae3e738fac333054d9
kazeemmuyideen <kazeemmuyideen@gmail.com>
2023-01-11 11:05:25 +0100

    Correction for Task 1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def element_at(my_list, idx):
-    # a function that replaces an element of a list at a specific position
-    # (like in C).
-
+    '''function that retrives an element from a like'''
     if idx < 0 or idx > (len(my_list) - 1):
-        return 'none'
-    else:
-        return(my_list[idx])
+        return None
+    return (my_list[idx])
